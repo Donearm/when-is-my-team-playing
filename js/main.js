324b74ba2e639673c8c4eb66a57480ee45814c8c
Gianluca Fiore <forod.g@gmail.com>
2018-11-08 09:46:30 +0100

    right-align standings headers

--- js/main.js
+++ js/main.js
@@ -86,7 +86,7 @@ $(document).ready(function() {
 				let localStandingsUpdated = new Date(parsedData.competition.lastUpdated);
 				let tableStructure = []; // array to contain the standings' table
 				// Header of the standings' table
-				let tableHeader = '<thead class="thead-dark"><tr><th scope="col">Team</th><th scope="col">Games</th><th scope="col">Won</th><th scope="col">Drawn</th><th scope="col">Lost</th><th scope="col">Points</th></tr></thead><tbody>';
+				let tableHeader = '<thead class="thead-dark"><tr><th scope="col">Team</th><th scope="col" class="text-right">Games</th><th scope="col" class="text-right">Won</th><th scope="col" class="text-right">Drawn</th><th scope="col" class="text-right">Lost</th><th scope="col" class="text-right">Points</th></tr></thead><tbody>';
 
 				$("#standings").empty();
 				$("#standings").append('<p>' + leagueName + ' standings as of ' + localStandingsUpdated.toString() + '</p>');
@@ -122,7 +122,7 @@ $(document).ready(function() {
 				let localStandingsUpdated = new Date(parsedData.competition.lastUpdated);
 				let tableStructure = []; // array to contain the standings' table
 				// Header of the standings' table
-				let tableHeader = '<thead class="thead-dark"><tr><th scope="col">Team</th><th scope="col">Games</th><th scope="col">Won</th><th scope="col">Drawn</th><th scope="col">Lost</th><th scope="col">Points</th></tr></thead><tbody>';
+				let tableHeader = '<thead class="thead-dark"><tr><th scope="col">Team</th><th scope="col" class="text-right">Games</th><th scope="col" class="text-right">Won</th><th scope="col" class="text-right">Drawn</th><th scope="col" class="text-right">Lost</th><th scope="col" class="text-right">Points</th></tr></thead><tbody>';
 
 				$("#standings").empty();
 				$("#standings").append('<p>' + leagueName + ' standings as of ' + localStandingsUpdated.toString() + '</p>');
